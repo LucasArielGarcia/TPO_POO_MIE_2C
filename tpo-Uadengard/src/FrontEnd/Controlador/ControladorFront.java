cf05181119cfff464f851dc03ea0a5a3b72f3497
Lucas Ariel García <85250463+LucasArielGarcia@users.noreply.github.com>
2024-11-06 20:21:03 -0300

    singleton al controlador

--- tpo-Uadengard/src/FrontEnd/Controlador/ControladorFront.java
+++ tpo-Uadengard/src/FrontEnd/Controlador/ControladorFront.java
@@ -3,18 +3,23 @@ package FrontEnd.Controlador;
 import BackEnd.Entidades.Ubicacion;
 import BackEnd.Juego.Juego;
 
-import javax.swing.*;
-import java.awt.*;
 import java.util.List;
 
 public class ControladorFront {
     private Juego juego = new Juego();
+    private static ControladorFront controladorFront;
     public void crearPersonaje(String nombre, String tipoHeroe){
         int tipo;
         if (tipoHeroe.equals("Guerrero")){tipo = 1;}
         else if (tipoHeroe.equals("Arquero")) {tipo = 2;}
         else {tipo = 3;}
         juego.crearHeroe(tipo,nombre);
+    }
+
+    public static ControladorFront getinstancia() {
+        if (controladorFront == null)
+            controladorFront = new ControladorFront();
+        return controladorFront;
     }
 
     public List<Ubicacion> abrirMapa(){
