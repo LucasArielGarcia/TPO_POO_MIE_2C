602140b4610935b67ad2b17d79252e80c0dd74d8
zambranolopez <zambranomal@gmail.com>
2024-11-10 18:27:28 -0300

    Se crea el constructor del MercaderView

--- tpo-Uadengard/src/BackEnd/Entidades/objectView/MercaderView.java
+++ tpo-Uadengard/src/BackEnd/Entidades/objectView/MercaderView.java
@@ -8,8 +8,8 @@ import BackEnd.Entidades.Items;
 public class MercaderView {
 	private List<Items> itemVenta = new ArrayList<>();
 	
-    public MercaderView(Items itemVenta) {
-    	this.itemVenta.add(itemVenta);
+    public MercaderView(List<Items> itemVenta) {
+    	this.itemVenta = itemVenta;
 	}
 	public List<Items> getMostrarCatologoItems(){
 		return this.itemVenta;
